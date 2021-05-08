5b76450c34e0846a76d8db10aca4ff4bd1a466cd
razieli <rzyuval@gmail.com>
2021-05-08 13:56:28 +0300

    Created the request and response messages for the prototype.

---
+++ Common/src/main/java/il/ac/haifa/cs/sweng/cms/common/messages/ResponseStatus.java
@@ -0,0 +1,11 @@
+package il.ac.haifa.cs.sweng.cms.common.messages;
+
+/**
+ * Statuses of responses.
+ *
+ * @author Yuval Razieli
+ */
+public enum ResponseStatus {
+    Acknowledged,
+    Declined
+}
