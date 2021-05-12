cec014dd4d576d88c5b59e7b1215de2b7899fce7
razieli <rzyuval@gmail.com>
2021-05-12 17:16:25 +0300

    Added handling of requests received by the server.

--- Common/src/main/java/module-info.java
+++ Common/src/main/java/module-info.java
@@ -2,5 +2,7 @@ module Common {
     requires java.persistence;
     exports il.ac.haifa.cs.sweng.cms.common.entities;
     exports il.ac.haifa.cs.sweng.cms.common.messages;
+    exports il.ac.haifa.cs.sweng.cms.common.messages.requests;
+    exports il.ac.haifa.cs.sweng.cms.common.messages.responses;
     exports il.ac.haifa.cs.sweng.cms.common.util;
 }
