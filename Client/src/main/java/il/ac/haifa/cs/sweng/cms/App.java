b938203733b45c18109bbacafb6fe6109b7b6b85
ShaySilbeklang <shay.silber@gmail.com>
2021-05-13 23:01:43 +0300

    pom update

--- Client/src/main/java/il/ac/haifa/cs/sweng/cms/App.java
+++ Client/src/main/java/il/ac/haifa/cs/sweng/cms/App.java
@@ -1,28 +1,48 @@
 package il.ac.haifa.cs.sweng.cms;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private static Scene scene;
+    private static Movie selectedFilmTitle ;
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("ViewMovies"), 600, 400);
         stage.setScene(scene);
         stage.show();
     }
 
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static Movie getSelectedFilmTitle() {
+        return selectedFilmTitle;
+    }
+
+    public static void setSelectedFilmTitle(Movie selectedFilmTitle) {
+        App.selectedFilmTitle = selectedFilmTitle;
+    }
+
     public static void main(String[] args) {
+        DB.main(args);
         launch();
     }
 
