c3e299c621faacd2f41b122434477ada464fc81f
ShaySilbeklang <shay.silber@gmail.com>
2021-05-13 20:20:17 +0300

    new Dev

--- Client/src/main/java/il/ac/haifa/cs/sweng/cms/PrimaryController.java
+++ Client/src/main/java/il/ac/haifa/cs/sweng/cms/PrimaryController.java
@@ -1,5 +1,14 @@
 package il.ac.haifa.cs.sweng.cms;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class PrimaryController {
+
+	  
+
 
 }
