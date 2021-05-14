6c2936035fc21c75c72011d4ebfb05656363dd46
ShaySilbeklang <shay.silber@gmail.com>
2021-05-14 10:40:31 +0300

    start of MovieEditController

--- Client/src/main/java/il/ac/haifa/cs/sweng/cms/MoveEditController.java
+++ Client/src/main/java/il/ac/haifa/cs/sweng/cms/MoveEditController.java
@@ -1,16 +1,25 @@
-import javafx.fxml.FXML;
+package il.ac.haifa.cs.sweng.cms;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+
 
 public class MovieEditController {
 	
 	private boolean gnerationFlag = false; 
-	@FXML
-    private VBox Vbox;
+
 	
     @FXML
     private Button helloBtn;
