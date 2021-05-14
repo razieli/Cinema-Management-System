9b326d7a41f69c6e6823ed742b91c3f6d131a8f5
ShaySilbeklang <shay.silber@gmail.com>
2021-05-14 17:23:37 +0300

    moved to IntelliJ

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module Client {
-    requires javafx.controls;
-<<<<<<< HEAD
+    //requires javafx.controls;
+
     requires Common;
-=======
+
 	requires javafx.fxml;
 	requires javafx.base;
->>>>>>> refs/heads/dev_Zvi
+
     exports il.ac.haifa.cs.sweng.cms;
 }
