e0f8c31f74b011eac1f826376ef42c6c246a5d6d
ShaySilbeklang <shay.silber@gmail.com>
2021-05-15 00:12:47 +0300

    IntelliJ changes

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module Client {
-    //requires javafx.controls;
 
     requires Common;
 
 	requires javafx.fxml;
 	requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.persistence;
+    requires org.hibernate.orm.core;
 
     exports il.ac.haifa.cs.sweng.cms;
 }
