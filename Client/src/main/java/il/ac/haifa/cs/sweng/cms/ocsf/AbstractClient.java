5fd28f2b323e47f19b22e6b9411060168e54e236
ShaySilbeklang <shay.silber@gmail.com>
2021-06-07 12:31:39 +0300

    Cancel Ticket update

--- Client/src/main/java/il/ac/haifa/cs/sweng/cms/ocsf/AbstractClient.java
+++ Client/src/main/java/il/ac/haifa/cs/sweng/cms/ocsf/AbstractClient.java
@@ -9,96 +9,96 @@ import java.net.*;
 import java.util.*;
 
 /**
-* The <code> AbstractClient </code> contains all the
-* methods necessary to set up the client side of a client-server
-* architecture.  When a client is thus connected to the
-* server, the two programs can then exchange <code> Object </code>
-* instances.<p>
-*
-* Method <code> handleMessageFromServer </code> must be defined by
-* a concrete subclass. Several other hook methods may also be
-* overriden.<p>
-*
-* Several public service methods are provided to
-* application that use this framework.<p>
-*
-* The modifications made to this class in version 2.2 are:
-* <ul>
-* <li> Method <code>sendToServer()</code> is not
-* declared final anymore. This allows user of the
-* framework to override it, perhaps to perform some
-* filtering before sending the message to the server.
-* However, any overriden version of this method
-* should include a call to the original one.
-* <li> A test is made before calling the
-* <code>handleMessageFromServer</code> method such that
-* when  <code>closeConnection</code> returns, it
-* is garanteed that no new messages will be handled.
-* </ul>
-* The modifications made to this class in version 2.31 are:
-* <ul>
-* <li> The <code>run()</code> method now calls the <code>connectionException</code> 
-* callback when an object of unknown class is received from the input stream
-* or when the message handler throw a <code>RuntimeException</code>.
-* <li> The <code>connectionClosed</code> callback might be called after
-* <code>connectionException</code> if the exception causes the end of te thread.
-* <li> The <code>clientReader</code> reference is set to <code>null</code>
-* earlier in <code>run()</code> method.
-* <li> The call to <code>connectionClosed</code> has been moved from
-* <code>closeConnection</code> to <code>run</code> method to garantee
-* that connection is really closed when this callback is called.
-* </ul><p>
-*
-* Project Name: OCSF (Object Client-Server Framework)<p>
-*
-* @author Dr. Robert Lagani&egrave;re
-* @author Dr. Timothy C. Lethbridge
-* @author Fran&ccedil;ois  B&eacutel;langer
-* @author Paul Holden
-* @version December 2003 (2.31)
-*/
+ * The <code> AbstractClient </code> contains all the
+ * methods necessary to set up the client side of a client-server
+ * architecture.  When a client is thus connected to the
+ * server, the two programs can then exchange <code> Object </code>
+ * instances.<p>
+ *
+ * Method <code> handleMessageFromServer </code> must be defined by
+ * a concrete subclass. Several other hook methods may also be
+ * overriden.<p>
+ *
+ * Several public service methods are provided to
+ * application that use this framework.<p>
+ *
+ * The modifications made to this class in version 2.2 are:
+ * <ul>
+ * <li> Method <code>sendToServer()</code> is not
+ * declared final anymore. This allows user of the
+ * framework to override it, perhaps to perform some
+ * filtering before sending the message to the server.
+ * However, any overriden version of this method
+ * should include a call to the original one.
+ * <li> A test is made before calling the
+ * <code>handleMessageFromServer</code> method such that
+ * when  <code>closeConnection</code> returns, it
+ * is garanteed that no new messages will be handled.
+ * </ul>
+ * The modifications made to this class in version 2.31 are:
+ * <ul>
+ * <li> The <code>run()</code> method now calls the <code>connectionException</code>
+ * callback when an object of unknown class is received from the input stream
+ * or when the message handler throw a <code>RuntimeException</code>.
+ * <li> The <code>connectionClosed</code> callback might be called after
+ * <code>connectionException</code> if the exception causes the end of te thread.
+ * <li> The <code>clientReader</code> reference is set to <code>null</code>
+ * earlier in <code>run()</code> method.
+ * <li> The call to <code>connectionClosed</code> has been moved from
+ * <code>closeConnection</code> to <code>run</code> method to garantee
+ * that connection is really closed when this callback is called.
+ * </ul><p>
+ *
+ * Project Name: OCSF (Object Client-Server Framework)<p>
+ *
+ * @author Dr. Robert Lagani&egrave;re
+ * @author Dr. Timothy C. Lethbridge
+ * @author Fran&ccedil;ois  B&eacutel;langer
+ * @author Paul Holden
+ * @version December 2003 (2.31)
+ */
 public abstract class AbstractClient implements Runnable
 {
 
 // INSTANCE VARIABLES ***********************************************
 
   /**
-  * Sockets are used in the operating system as channels
-  * of communication between two processes.
-  * @see java.net.Socket
-  */
+   * Sockets are used in the operating system as channels
+   * of communication between two processes.
+   * @see java.net.Socket
+   */
   private Socket clientSocket;
 
   /**
-  * The stream to handle data going to the server.
-  */
+   * The stream to handle data going to the server.
+   */
   private ObjectOutputStream output;
 
   /**
-  * The stream to handle data from the server.
-  */
+   * The stream to handle data from the server.
+   */
   private ObjectInputStream input;
 
   /**
-  * The thread created to read data from the server.
-  */
+   * The thread created to read data from the server.
+   */
   private Thread clientReader;
 
   /**
-  * Indicates if the thread is ready to stop.
-  * Needed so that the loop in the run method knows when to stop
-  * waiting for incoming messages.
-  */
+   * Indicates if the thread is ready to stop.
+   * Needed so that the loop in the run method knows when to stop
+   * waiting for incoming messages.
+   */
   private boolean readyToStop= false;
 
   /**
-  * The server's host name.
-  */
+   * The server's host name.
+   */
   private String host;
 
   /**
-  * The port number.
-  */
+   * The port number.
+   */
   private int port;
 
 // CONSTRUCTORS *****************************************************
@@ -183,8 +183,8 @@ public abstract class AbstractClient implements Runnable
   final public void closeConnection() throws IOException
   {
 
-      readyToStop= true; 
-      closeAll();
+    readyToStop= true;
+    closeAll();
   }
 
 // ACCESSING METHODS ------------------------------------------------
@@ -270,9 +270,9 @@ public abstract class AbstractClient implements Runnable
         // Get data from Server and send it to the handler
         // The thread waits indefinitely at the following
         // statement until something is received from the server
-        
+
         try { // added in version 2.31
-        
+
           msg = input.readObject();
 
           // Concrete subclasses do what they want with the
@@ -280,16 +280,16 @@ public abstract class AbstractClient implements Runnable
           if (!readyToStop) {  // Added in version 2.2
             handleMessageFromServer(msg);
           }
-          
+
         } catch(ClassNotFoundException ex) { // when an unknown class is received
-        
+
           connectionException(ex);
-          
+
         } catch (RuntimeException ex) { // thrown by handleMessageFromServer
-        
+
           connectionException(ex);
         }
-      } 
+      }
     }
     catch (Exception exception)
     {
@@ -301,13 +301,13 @@ public abstract class AbstractClient implements Runnable
         }
         catch (Exception ex) { }
 
-        clientReader = null; 
-        connectionException(exception);      
+        clientReader = null;
+        connectionException(exception);
       }
     } finally {
-    
-        clientReader = null; 
-        connectionClosed();   // moved here in version 2.31
+
+      clientReader = null;
+      connectionClosed();   // moved here in version 2.31
     }
   }
 
